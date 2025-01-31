66134fb826162de5e8d802ed05353de0c34e31a0
VivekPingali <vivekyy65@gmail.com>
2025-01-31 23:19:13 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
+import Chatbot from './components/Chatbot';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-900">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Chatbot />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
